432c4fa54287e2adf7935da69869e83a748dc762
TommyFurgi <tomcio.220903@gmail.com>
2024-03-25 19:18:08 +0100

    lab4 small changes

--- lab4/measure_time.jl
+++ lab4/measure_time.jl
@@ -1,7 +1,53 @@
 using Interpolations, Polynomials, DataFrames, CSV
 
-include("divided_differences.jl")
-include("lagnage.jl")
+function NewtonInterpolation(xs, ys)
+    T = Float64[]
+    n = size(xs)[1]
+    
+    # Populate the divided difference table
+    for a in 1:n
+        push!(T, ys[a])
+        for i in 1:a-1
+            nom = T[end] - T[end - a + 1] # numerator of the divided difference
+            den = xs[a] - xs[a - i]      # denominator of the divided difference
+            push!(T, nom / den)          # push the divided difference onto T
+        end
+    end
+    
+    # Extract the coefficients of the interpolating polynomial
+    newt = Float64[]
+    k = 0
+    for i in 1:n
+        k = k + i
+        push!(newt, T[k])  # Store the coefficients
+    end
+    return newt
+end
+
+function Newton(newt, xs, x)
+    n = size(xs)[1]    # Size of xs, not xsf
+    b = newt[n]        # Start with the last coefficient
+    for i in 1:n-1
+        b = newt[n-i] + b * (x - xs[n-i])  # Update b using the recursive Newton formula
+    end
+    return b
+end
+
+function Lagrange_interpolation(xs, ys, x)
+    n = size(xs)[1]
+    result = 0
+    for i in 1:n
+        L = 1
+        for j in 1:n
+            if j != i
+                L *= (x-xs[j])/(xs[i]-xs[j])
+            end
+        end
+        result += (ys[i]*L)
+    end
+    return result
+end
+
 
 xs = 0:1:10
 ys = [rand() for _ in xs]
